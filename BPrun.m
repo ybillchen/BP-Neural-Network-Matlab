7619d5735e406bc75d5eea3ef4af66eb30140e2c
Bill <33487119+EnthalpyBill@users.noreply.github.com>
2018-09-10 09:47:57 +0800

    Update BPrun.m

--- BPrun.m
+++ BPrun.m
@@ -1,9 +1,5 @@
-function vout_ = BPrun(lin_, lout_, vin_, activationFunction)
+function vout_ = BPrun(lin_, lout_, vin_)
 % function 'BPrun' gives the ouput vector of a BP neural network
-if activationFunction == 'tanh'
-    vout_ = lout_.run(tanh(lin_.run(vin_)));
-elseif activationFunction == 'sigmoid'
-    vout_ = lout_.run(sigmoid(lin_.run(vin_)));
-end
+vout_ = lout_.run(sigmoid(lin_.run(vin_)));
 end
 
